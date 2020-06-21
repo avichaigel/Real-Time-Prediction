9de722b244df185457c93813c3e0c6df20eb9a6c
Israel Shai <israel.shai@live.biu.ac.il>
2020-06-21 23:14:38 +0300

    graph parameters

--- ex2.py
+++ ex2.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-import gplearn as gp
 import numpy as np
-
+import gplearn as gp
+import math
 REQUIRED_ROW_LENGTH = 1000
 UNDEFINED = -1
 
@@ -27,10 +27,56 @@ class Sample:
 
 
 class RowData:
-    def __init__(self, tuple_list_of_samples, m1, m2):
+
+    def __init__(self, tuple_list_of_samples):
         self.tuple_list_of_samples = tuple_list_of_samples
-        self.m1 = m1
-        self.m2 = m2
+        self.__x1_data = UNDEFINED
+        self.__x2_data = UNDEFINED
+        self.__x3_data = UNDEFINED
+        self.__x4_data = UNDEFINED
+        self.__graphs_covering = UNDEFINED  # 0-1
+        self.__graph1_scattered = UNDEFINED  # 0-1
+        self.__graph2_scattered = UNDEFINED  # 0-1
+        self.__graphs_scattered = UNDEFINED  # 0-1
+
+    def set_x1_data(self, list_of_data):
+        self.__x1_data = list_of_data
+
+    def set_x2_data(self, list_of_data):
+        self.__x2_data = list_of_data
+
+    def set_x3_data(self, list_of_data):
+        self.__x3_data = list_of_data
+
+    def set_x4_data(self, list_of_data):
+        self.__x4_data = list_of_data
+
+    def set_graphs_covering(self, val):
+        self.__graphs_covering = val
+
+    def set_graph1_scattered(self, val):
+        self.__graph1_scattered = val
+
+    def set_graph2_scattered(self, val):
+        self.__graph2_scattered = val
+
+    def set_graphs_scattered(self, val):
+        self.__graphs_scattered = val
+
+
+class ListData:
+
+    def __init__(self, list):
+        self.__list = list
+        self.__first_twenty = [list[i] for i in range(20)]
+        self.__last_ten = [list[i] for i in range(20, 30)]
+        self.__mean = np.mean(list)
+        self.__first_twenty_mean = np.mean(self.__first_twenty)
+        self.__last_ten_mean = np.mean(self.__last_ten)
+
+    def get_mean(self):
+        return self.__mean
+
 
 
 def create_columns():
@@ -74,14 +120,14 @@ def read_data(row_dictionary):
     # create dictionary of the row of listOfNodes : anomaly
     row_number = 0
 
-    for i in range(REQUIRED_ROW_LENGTH ):
+    for i in range(REQUIRED_ROW_LENGTH):
         # anomaly = 0/1
         anomaly = anomalies[row_number]
         list_of_samples = []
         for j in range(0, 120, 4):
-            sample = Sample(columns[j][i], columns[j+1][i], columns[j+2][i], columns[j+3][i])
+            sample = Sample(columns[j][i], columns[j + 1][i], columns[j + 2][i], columns[j + 3][i])
             list_of_samples.append(sample)
-        row_data = RowData(tuple(list_of_samples),UNDEFINED, UNDEFINED)
+        row_data = RowData(tuple(list_of_samples))
         row_dictionary[row_data] = anomaly
         row_number += 1
 
@@ -100,12 +146,32 @@ def generate_slopes(row_dictionary):
             a2_list.append(sample.get_value2())
             a3_list.append(sample.get_value3())
             a4_list.append(sample.get_value4())
-            if len(a1_list) == 15:
-                plt.plot(a2_list, a1_list)
-                plt.show()
-                plt.plot(a4_list, a3_list)
-                plt.show()
-                plt.close()
+
+        list1_data = ListData(a1_list)
+        row_data.set_x1_data(list1_data)
+        list2_data = ListData(a2_list)
+        row_data.set_x2_data(list2_data)
+        list3_data = ListData(a3_list)
+        row_data.set_x3_data(list3_data)
+        list4_data = ListData(a4_list)
+        row_data.set_x4_data(list4_data)
+
+        a = (np.std(a1_list) + np.std(a2_list)) / 2
+        row_data.set_graph1_scattered(a)
+        b = (np.std(a3_list) + np.std(a4_list)) / 2
+        row_data.set_graph2_scattered(b)
+        row_data.set_graphs_scattered((a+b)/2)
+
+        # graph_1_center = [b, a]
+        e = list1_data.get_mean()
+        f = list2_data.get_mean()
+        # graph_2_center = [d, c]
+        c = list3_data.get_mean()
+        d = list4_data.get_mean()
+
+        # how much the graphs are covering each other - the distance between their centers
+        row_data.set_graphs_covering(math.sqrt((f-d)**2+(e-c)**2))
+
         plt.plot(a2_list, a1_list)
         plt.show()
         plt.plot(a4_list, a3_list)
@@ -118,7 +184,7 @@ def generate_slopes(row_dictionary):
 
 
 # def train(rows):
-    # gp.genetic.SymbolicRegressor(population_size=5, const_range=(-10,10), function_set=('add','sub'),
+# gp.genetic.SymbolicRegressor(population_size=5, const_range=(-10,10), function_set=('add','sub'),
 #                                  )
 
 def main():
@@ -132,5 +198,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
